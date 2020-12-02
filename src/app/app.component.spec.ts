9107ab519a2be3b77ba62b5b7173046dc166140a
Viacheslav Bondarchuk <slava.777.bondarchuk@outlook.com>
2020-12-03 00:30:40 +0200

    was corrected settiong of project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-free-demo'`, () => {
+  it(`should have as title 'File-space'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-free-demo');
+    expect(app.title).toEqual('File-space');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-free-demo app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('File-Space app is running!');
   });
 });
